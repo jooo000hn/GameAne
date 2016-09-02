8514a982bd564fcb1eaf6df06a7b912dfa5a8116
l1fan <yiyimx@gmail.com>
2016-09-02 22:58:10 +0800

    ht sdk ios

---
+++ sdks/ht_ios/HTSDK.framework/Headers/HTSDK.h
@@ -0,0 +1,21 @@
+//
+//  HTSDK.h
+//  HTSDK
+//
+//  Created by 王璟鑫 on 16/4/25.
+//  Copyright © 2016年 王璟鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTSDK.
+FOUNDATION_EXPORT double HTSDKVersionNumber;
+
+//! Project version string for HTSDK.
+FOUNDATION_EXPORT const unsigned char HTSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTSDK/PublicHeader.h>
+
+#import <HTSDK/HTConnection.h>
+
+#import <HTSDK/HTIAPManager.h>
