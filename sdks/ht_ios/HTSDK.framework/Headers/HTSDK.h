0237755aba6729b969c6c3785ae50732f9709704
l1fan <yiyimx@gmail.com>
2017-02-23 16:41:02 +0800

    update ht ios sdk

--- sdks/ht_ios/HTSDK.framework/Headers/HTSDK.h
+++ sdks/ht_ios/HTSDK.framework/Headers/HTSDK.h
@@ -2,7 +2,7 @@
 //  HTSDK.h
 //  HTSDK
 //
-//  Created by 王璟鑫 on 16/4/25.
+//  Created by 王璟鑫 on 2016/11/23.
 //  Copyright © 2016年 王璟鑫. All rights reserved.
 //
 
@@ -16,6 +16,7 @@ FOUNDATION_EXPORT const unsigned char HTSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <HTSDK/PublicHeader.h>
 
-#import <HTSDK/HTConnection.h>
+#import "HTConnect.h"
+#import "HTIAPManager.h"
+#import "HTAssistiveTouch.h"
 
-#import <HTSDK/HTIAPManager.h>
